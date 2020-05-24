e7a7d57c3d1d11052c8233462f2a529b76898750
healyyyyyy <54723263+healyyyyyy@users.noreply.github.com>
2020-05-24 19:52:22 +0500

    Integrated Webscraper and search results with html
    Now when, the user searches enters a search query, the website takes it and runs it through the scraper. The scraper searches google for the exact input by the user (this is all happening in the back-end with no mention of google to the user). It then takes all the links available on google's search results, strips them down to just the hyper links and sends those hyperlinks back to the program.
    
    Then, the program iteratively takes each individual link and slices away any suffix and prefixes that googled added as a security measure (they do this to break links so you can't use them). All the (fully-functioning) hyper links are then added to a dictionary and printed on to the next page with indexing.

--- Website/main.py
+++ Website/main.py
@@ -1,7 +1,55 @@
 from flask import Flask, render_template, request
+import requests, webbrowser
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+def scrape(text):
+    user_search = text
+    print("Running your search...")
+    print('')
+    print('')
+
+    #The get function gets all the data from the page link we enter.
+    gsearch = requests.get("https://www.google.com/search?q="+user_search)
+
+    #This function converts the extracted data into html parser format, Ez to read.
+    soup = BeautifulSoup(gsearch.text, 'html.parser')
+    #print(soup.prettify())
+
+    #We select the class and object within the html code to print out only
+    #select parts of it, allowing us to extract only the URL.
+    result = soup.select('.BNeawe a')
+
+    dict={}
+    n=0
+    for i in result[0:11]: #loop to get first 10 links.
+        initial_link = i.get('href')
+        if "http" in initial_link:
+            slicer = initial_link.find("http")
+            initial_link = initial_link[int(slicer):]
+            if "&sa" in initial_link:
+                slicer = initial_link.find("&sa")
+                initial_link = initial_link[:int(slicer)]
+            elif "%3Fref" in initial_link:
+                slicer = initial_link.find("%3Fref")
+                initial_link = initial_link[:int(slicer)]
+            else:
+                pass
+        elif "www." in initial_link:
+            slicer = initial_link.find("www.")
+            initial_link = initial_link[int(slicer):]
+            if "&sa" in initial_link:
+                slicer = initial_link.find("&sa")
+                initial_link = initial_link[:int(slicer)]
+            elif "%3Fref" in initial_link:
+                slicer = initial_link.find("%3Fref")
+                initial_link = initial_link[:int(slicer)]
+            else:
+                pass
+        dict[n]=initial_link
+        n+=1
+    return dict
 
 @app.route('/')
 def index():
