afe8b07938852c40d7e203cc676f00cf084ab5bb
sabahismail <47879803+sabahismail@users.noreply.github.com>
2020-06-23 20:35:30 +0500

    Update Search.py
    reworking some things

--- Search/Search.py
+++ Search/Search.py
@@ -1,15 +1,50 @@
-class Search:
+import requests
+from bs4 import BeautifulSoup
 
-    def __init__:
-        pass
+def scrape(text):
+    user_search = text
+    print("Running your search...")
+    print('')
+    print('')
 
-    def sort():
-        pass
+    #The get function gets all the data from the page link we enter.
+    gsearch = requests.get("https://www.google.com/search?q="+user_search)
 
-    def similarity():
-        pass
+    #This function converts the extracted data into html parser format, Ez to read.
+    soup = BeautifulSoup(gsearch.text, 'html.parser')
+    #print(soup.prettify())
 
-    def rating():
-        pass
+    #We select the class and object within the html code to print out only
+    #select parts of it, allowing us to extract only the URL.
+    result = soup.select('.BNeawe a')
 
-        
+    #Creating a dictionary to present an organized results screen.
+    dict={}
+    n=0
+    for i in result[0:26]: #loop to get first 10 links.
+        initial_link = i.get('href') #grab the base url from our scraped data.
+        if "http" in initial_link: #Remove unwanted prefixes and suffixes from urls.
+            slicer = initial_link.find("http")
+            initial_link = initial_link[int(slicer):]
+            if "&sa" in initial_link:
+                slicer = initial_link.find("&sa")
+                initial_link = initial_link[:int(slicer)]
+            elif "%3Fref" in initial_link:
+                slicer = initial_link.find("%3Fref")
+                initial_link = initial_link[:int(slicer)]
+        elif "www." in initial_link:
+            slicer = initial_link.find("www.")
+            initial_link = initial_link[int(slicer):]
+            if "&sa" in initial_link:
+                slicer = initial_link.find("&sa")
+                initial_link = initial_link[:int(slicer)]
+            elif "%3Fref" in initial_link:
+                slicer = initial_link.find("%3Fref")
+                initial_link = initial_link[:int(slicer)]
+        for x in lst1:
+            flag = initial_link.find(lst1[x])
+        dict[n] = initial_link
+        #n+=1
+    return dict
+
+print(scrape("corona"))
