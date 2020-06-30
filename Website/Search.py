8188ba140fec378031892722d6d07f939fa9821f
AffanNemat <43030002+AffanNemat@users.noreply.github.com>
2020-06-30 23:17:29 +0500

    Update Search.py

--- Website/Search.py
+++ Website/Search.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 def scrape(text):
+    lst = []
+    #for text in list_of_words:
     user_search = text
-    print("Running your search...")
-    print('')
-    print('')
+    #print("Running your search...")
+    #print('')
+    #print('')
 
     # The get function gets all the data from the page link we enter.
     gsearch = requests.get("https://www.google.com/search?q=" + user_search)
@@ -13,24 +16,14 @@ def scrape(text):
     # This function converts the extracted data into html parser format, Ez to read.
     soup = BeautifulSoup(gsearch.text, 'html.parser')
     # print(soup.prettify())
-    soupier = BeautifulSoup(r_word, 'html.parser')
+    soupier = BeautifulSoup(r_word.text, 'html.parser')
     # We select the class and object within the html code to print out only
     # select parts of it, allowing us to extract only the URL.
     # print (soup)
     # result = soup.select('.BNeawe a')
     tags = soup.find_all('a')
-
     # Extracting URLs from the attribute href in the <a> tags.
 
-    # for tag in tags:
-    #   print ("--------")
-    #   print(tag.get('href'))
-
-    # print(result)
-    # for i in result:
-    #    print (i)
-    # Creating a dictionary to present an organized results screen.
-    lst = []
     for i in tags:  # loop to get first 10 links.
         initial_link = i.get('href')  # grab the base url from our scraped data.
         #print("initial link is : ", initial_link)
@@ -54,6 +47,7 @@ def scrape(text):
                 slicer = initial_link.find("%3Fref")
                 initial_link = initial_link[:int(slicer)]
             lst.append(initial_link)
+    #print(lst)
     return lst
     #lst = []
     #grbg = ["http", "&sa", "%3Fref", "/search?ie=UTF-8&q=COVID-19"]
@@ -94,6 +88,7 @@ def sort(a):
     added_in_lst1 = False
     added_in_lst2 = False
     for i in a:
+        print(i)
         for j in lst1:
             if j in i:
                 dict.get(0).append(i)
@@ -109,9 +104,9 @@ def sort(a):
                 dict.get(-1).append(i)
         added_in_lst1 = False
         added_in_lst2 = False
-    for i in dict[1]:
-        imp.append(dict[1][x])
-    return imp
+    #for i in dict[1]:
+        #imp.append(dict[1][x])
+    return dict
 
 def helper2(x):
     if len(x) < 100:
@@ -120,7 +115,47 @@ def helper2(x):
         return x
 
 
-# out = (scrape("habib university"))
-# print(out)
-# print(len(out))
-# print(sort(scrape("habib university")))
+def get_related_keywords(text):
+    user_search = text
+    #print("Running your search...")
+    #print('')
+    #print('')
+    lst_of_related_words = []
+    # The get function gets all the data from the page link we enter.
+    gsearch = requests.get("https://api.bing.com/osjson.aspx?query=" + user_search)
+    for i in gsearch.text.split(","):
+        i = i.replace('"',"").replace('[',"").replace(']', "")
+        lst_of_related_words.append(i)
+    return lst_of_related_words[1:]
+
+
+def scrape_on_keywords(related_keywords):
+    final_lst = []
+    total_links = 0
+    for a in related_keywords:
+        #print(a)
+        out = (scrape(a))
+        #print(out)
+        total_links += len(out)
+        my_dict = sort(out)
+        #print(my_dict)
+        for item in process_dict(my_dict):
+            final_lst.append(item)
+    #print(final_lst)
+    #print(len(final_lst))
+    #print(total_links)
+    return final_lst
+
+def process_dict(my_dict : dict):
+    mylst = my_dict.get(1)
+    #print(mylst)
+    return mylst
+
+
+text = "cat"
+related_words = [text]
+for word in get_related_keywords(text):
+    related_words.append(word)
+#print("all keywords are: ", related_words)
+
+scrape_on_keywords(related_words)
